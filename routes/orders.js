d552c36f6c9f3261b2cd2ad92d486127e2119b1d
Kayla Wiest <Kayla Wiest>
2016-11-14 17:45:15 -0500

    Delete capability

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var Order = require('../models/order');
 
-/* Get handler for /order */
+/* Get handler for /orders */
 router.get('/', function (req, res, next) {
    Order.find(function(err, orders){
        if(err){
@@ -18,14 +18,14 @@ router.get('/', function (req, res, next) {
    });
 });
 
-/* Get handler for /order/add */
+/* Get handler for /orders/add */
 router.get('/add', function(req, res, next){
    res.render('add', {
         title: 'New Order'
    });
 });
 
-/* Post handler for /order/add */
+/* Post handler for /orders/add */
 router.post('/add', function(req, res, next){
     Order.create({
         firstName: req.body.firstName,
@@ -44,6 +44,19 @@ router.post('/add', function(req, res, next){
             res.redirect('/orders');
         }
     })
+});
+
+/* GET /orders/delete/:_id */
+router.get('/delete/:_id', function(req, res, next) {
+    var _id = req.params._id;
+
+    Order.remove( { _id: _id }, function(err) {
+        if (err) {
+            console.log(err);
+            res.render('error', {message: 'Delete Error'});
+        }
+        res.redirect('/orders');
+    });
 });
 
 module.exports = router;
